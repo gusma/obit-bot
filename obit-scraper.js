9caf28d7e9877ee595f117e61cb2044f3186f2a6
Gustavo Malamud <gustavo.malamud@gmail.com>
2023-08-21 11:49:39 -0300

    Modify output folder

--- obit-scraper.js
+++ obit-scraper.js
@@ -76,7 +76,7 @@ async function generateRSS() {
       </channel>
     </rss>`;
 
-  fs.writeFileSync("output/rss.xml", rssFeed); // Write to rss.xml file
+  fs.writeFileSync("docs/rss.xml", rssFeed); // Write to rss.xml file
 }
 
 generateRSS();
