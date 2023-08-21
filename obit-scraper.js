e1bcb31a3928900e37f94aa997b4e2fe3f726760
Gustavo Malamud <gustavo.malamud@gmail.com>
2023-08-21 16:54:40 -0300

    Add today and yesterday to feed flow

--- obit-scraper.js
+++ obit-scraper.js
@@ -2,19 +2,27 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require("fs");
 
-const baseUrl =
-  "https://servicios.lanacion.com.ar/edicion-impresa/avisos-funebres/resultado/categorias=1041,1037,1036,1039,1035,1042,1040,7992-fecha=";
-// Format date to match url format eg. (YYYYMMDD)
-const currentDate = new Date().toISOString().split("T")[0].replace(/-/g, "");
+const baseUrl = "https://servicios.lanacion.com.ar/edicion-impresa/avisos-funebres/resultado/categorias=1041,1037,1036,1039,1035,1042,1040,7992-fecha=";
 
-async function scrapePage(pageNumber) {
+function formatDateToISO(yymmdd) {
+  return `${yymmdd.substring(0, 4)}-${yymmdd.substring(4, 6)}-${yymmdd.substring(6, 8)}`;
+}
+
+function getYesterday() {
+  const date = new Date();
+  date.setDate(date.getDate() - 1);
+  return date.toISOString().split("T")[0].replace(/-/g, "");
+}
+
+const currentDate = new Date().toISOString().split("T")[0].replace(/-/g, "")
+
+async function scrapePage(pageNumber, dateToScrape) {
   const results = [];
   try {
-    const url = `${baseUrl}${currentDate}-pagina=${pageNumber}-palabra=`;
+    const url = `${baseUrl}${dateToScrape}-pagina=${pageNumber}-palabra=`;
     const { data } = await axios.get(url);
     const $ = cheerio.load(data);
 
-    // Browse page content subsection
     $("#contenido .funebres").each((i, element) => {
       const itemText = $(element).text().trim();
       let name, description;
@@ -23,25 +31,14 @@ async function scrapePage(pageNumber) {
       name = tempName;
       description = descriptionParts.join(" - ");
 
-      // If there's an ul.acumulados, then append its text to description.
       const acumuladoUl = $(element).find("ul.acumulados");
       if (acumuladoUl.length > 0) {
         description += " " + acumuladoUl.text().trim();
       }
 
-      // Remove unnecessary wishing messages eg QEPD, RIP, etc.
-      const polishedName = name
-        .replace(/,\s?q\.e\.p\.d\.|,\s?Z\.L\.|,\s?QEPD|,\s?RIP/gi, "")
-        .trim();
+      const polishedName = name.replace(/,\s?q\.e\.p\.d\.|,\s?Z\.L\.|,\s?QEPD|,\s?RIP/gi, "").trim();
 
-      function formatDateToISO(yymmdd) {
-        return `${yymmdd.substring(0, 4)}-${yymmdd.substring(
-          4,
-          6
-        )}-${yymmdd.substring(6, 8)}`;
-      }
-
-      const date = formatDateToISO(currentDate);
+      const date = formatDateToISO(dateToScrape);
 
       results.push({
         name: polishedName,
@@ -51,7 +48,7 @@ async function scrapePage(pageNumber) {
     });
 
     if ($(`a[href$="-pagina=${pageNumber + 1}-palabra="]`).length > 0) {
-      results.push(...(await scrapePage(pageNumber + 1)));
+      results.push(...(await scrapePage(pageNumber + 1, dateToScrape)));
     }
 
     return results;
@@ -61,20 +58,25 @@ async function scrapePage(pageNumber) {
   return results;
 }
 
-// Generate RSS feed. No links included since funerary pages has no further deeplinking.
-async function generateRSS() {
-  const items = await scrapePage(1);
+async function generateRSS(date) {
+  const items = await scrapePage(1, date);
   let rssItems = "";
   items.forEach((item) => {
     rssItems += `
-        <item>
-          <title>${item.name}</title>
-          <description>${item.description}</description>
-          <pubDate>${new Date(item.date).toUTCString()}</pubDate>
-        </item>`;
+      <item>
+        <title>${item.name}</title>
+        <description>${item.description}</description>
+        <pubDate>${new Date(item.date).toUTCString()}</pubDate>
+      </item>`;
   });
 
-  // Properly format file
+  return rssItems;
+}
+
+(async function() {
+  const todayItems = await generateRSS(currentDate);
+  const yesterdayItems = await generateRSS(getYesterday());
+
   const rssFeed = `
     <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
       <channel>
@@ -87,11 +89,11 @@ async function generateRSS() {
        />
         <link>https://servicios.lanacion.com.ar/edicion-impresa/avisos-funebres/</link>
         <description>Funebres scraped from La Nación</description>
-        ${rssItems}
+        ${todayItems}
+        ${yesterdayItems}
       </channel>
     </rss>`;
 
-  fs.writeFileSync("docs/rss.xml", rssFeed); // Write to rss.xml file
-}
+  fs.writeFileSync("docs/rss.xml", rssFeed); 
+})();
 
-generateRSS();
